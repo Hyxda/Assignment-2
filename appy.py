de3171b7687b91f05388b0426528c48e0d0f8075
Hyxda <hyxder@icloud.com>
2023-03-19 14:38:44 -0400

    Added validation functions for code & name

--- appy.py
+++ appy.py
@@ -3,31 +3,31 @@ import random
 class Product():
     def __init__(self):
         self.product_code = self.get_product_code()
-        # self.product_name = self.get_product_name()
-        # self.product_price = self.get_product_price()
+        self.product_name = self.get_product_name()
+        self.product_price = self.get_product_price()
         # self.product_manuf = self.get_product_manuf()
         # self.stock_level = self.get_stock_level()
         # self.monthly_est = self.get_monthly_est()
 
-    # def get_product_code(self):
-    #     while True:
-    #         product_code = int(input("Enter the product code (between 100 and 1000): "))
-    #         if product_code < 100 or product_code > 1000:
-    #             print("Product code must be between 100 and 1000.")
-    #         else:
-    #             return product_code
+    def get_product_code(self):
+        while True:
+            product_code = input("Enter the product code (between 100 and 1000): ")
+            if product_code.isdigit() and 100 <= int(product_code) <= 1000:
+                return int(product_code)
+            else:
+                print("Incorrect input, please re-enter Product Code: ")
 
-#     def get_product_name(self):
-#         while True:
-#             product_name = input("Enter Product Name: ")
-#             if product_name.isalpha() == False:
-#                 product_name = input("Incorrect input, please re-enter Product Name:")
-#             else:
-#                 return product_name
-    
-#     def get_product_price(self):
-#         pass
+    def get_product_name(self):
+        while True:
+            product_name = input("Enter Product Name: ")
+            if product_name.isalpha():
+                return product_name
+            else:
+                print("Incorrect input, please re-enter Product Name: ")
 
-# product = Product()
+    def get_product_price(self):
+        pass
 
-# print(product.product_name)
+product = Product()
+
+print(product.product_name)
